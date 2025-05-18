ae8d8d710c6cb64bcd0c0db7813c66905648f6c1
leraH <LeraArt111@gmail.com>
2025-05-18 23:02:51 +0300

    unit тест

---
+++ test/TestUnit.java
@@ -0,0 +1,24 @@
+import demo.parallel.Complex;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+public class ComplexTest {
+    private Complex a;
+    private Complex b;
+
+    @BeforeEach
+    void createComplex() {
+        a = new Complex(20.3, 15.15);
+        b = new Complex(18.1, 19.2);
+    }
+
+    @Test
+    void subtractTest() {
+        Complex result = a.subtract(b);
+
+        Assertions.assertEquals(20.3 - 15.15, result.getRe(), 0.00001);
+        Assertions.assertEquals(18.1 - 19.2, result.getIm(), 0.00001);
+    }
+
+}
